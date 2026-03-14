57c8d57f4a8d1942767f350539b3621d4ad82780
agokhanakin-blip <agokhanakin@gmail.com>
2026-03-14 13:51:00 +0300

    Update games.js

--- games.js
+++ games.js
@@ -1,26 +1,31 @@
-function loadGames(type, containerId){
+function loadGames(type,containerId){
 
 fetch("https://tyroneunblockedgames.com/generate_github_games_json.php?type="+type)
 
-.then(res => res.json())
+.then(res=>res.json())
 
-.then(data => {
+.then(data=>{
 
-const container = document.getElementById(containerId)
+const container=document.getElementById(containerId)
 
 container.innerHTML=""
 
-data.forEach(game => {
+data.forEach(game=>{
 
-const card = document.createElement("div")
+const card=document.createElement("article")
 
 card.className="game-card"
 
-card.innerHTML = `
+card.innerHTML=`
+
 <a href="${game.link}" target="_blank">
+
 <img src="${game.thumb}" alt="${game.name}">
+
 <h3>${game.name}</h3>
+
 </a>
+
 `
 
 container.appendChild(card)
